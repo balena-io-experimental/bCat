585a69911530a09a263204ad1a938b7fd01333c5
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2022-05-04 19:57:58 +0530

    Cleanup committed repos
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# install yq 
+
+wget https://github.com/mikefarah/yq/releases/download/v4.25.1/yq_linux_amd64.tar.gz -O - | tar xz && mv yq_linux_amd64 /usr/bin/yq
+
+
 
 
 yq '. *= load("docker-compose.test.yml")' docker-compose.yml > a.yml
