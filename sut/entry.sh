fdc1119d67e895975b7bba2db72463a67e9ca950
Akis Kesoglou <akiskesoglou@gmail.com>
2022-05-06 08:49:24 +0300

    Correctly invoke SUT entry script

--- sut/entry.sh
+++ sut/entry.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
+set -ex
+
 recordedFile=/usr/src/result.wav
 
 echo "Starting SUT..."
 
-# chmod +x test.sh 
+# chmod +x test.sh
 # ./test.sh
 
 # result=$?
 
 # echo "SUT exited with code $result"
 
-releaseID=$(curl --silent "${BALENA_SUPERVISOR_ADDRESS}/v2/applications/state?apikey=${BALENA_SUPERVISOR_API_KEY}" | jq .[\"bcat-proto\"][\"services\"][\"sut\"][\"releaseId\"])
+release_id=$(curl "${BALENA_SUPERVISOR_ADDRESS}/v2/applications/${BALENA_APP_ID}/state?apikey=${BALENA_SUPERVISOR_API_KEY}" \
+    | jq -r '.local[].services[].releaseId')
 
-echo ${releaseID}
+echo ${release_id}
 
 #  Updating release tag
-curl -X POST --silent --compressed \
+curl -X POST --compressed \
 "https://api.balena-cloud.com/v6/release_tag" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer ${BALENACLOUD_API_KEY}" \
 --data '{
-    "release": "${releaseID}",
+    "release": "${release_id}",
     "tag_key": "test_result",
     "value": "${result}"
 }'
 
 echo "Updated release tag with exit code $result"
 
-# Upload files to temporary storage 
+# Upload files to temporary storage
 
 if test -f "${recordedFile}"; then
     echo "Uploading ${recordedFile} exists."
